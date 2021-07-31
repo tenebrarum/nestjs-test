1e873df88dfa2b59572d024196896e9e80a4ad28
Chris <tenebrarum91@gmail.com>
2021-07-31 16:25:41 +0200

    initial commit

---
+++ src/cats.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { CreateCatDto } from './dto/CreateCatDto';
+import { Cat } from './interfaces/cat.interface';
+
+@Injectable()
+export class CatsService {
+  private readonly cats: Cat[] = [];
+
+  findByName(name: string): Cat {
+    return this.cats.find((cat) => cat.name === name);
+  }
+
+  create(createDto: CreateCatDto): Cat {
+    this.cats.push(createDto);
+    return createDto;
+  }
+
+  findAll(): Cat[] {
+    return this.cats;
+  }
+}
